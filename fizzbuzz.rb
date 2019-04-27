92cb856ef5a555fd0e4094aac4ca127dc7a1121d
beta-scanner-6136 <mahdi.saeikhalili@gmail.com>
2019-04-27 14:15:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,9 @@
 def fizzbuzz(n)
   if n % 15 == 0
     return "FizzBuzz"
-    elif n % 3 == 0 
+    elsif n % 3 == 0 
     return "Fizz"
-    elif n % 5 == 0 
+    elsif n % 5 == 0 
     return "Buzz"
     
   end
